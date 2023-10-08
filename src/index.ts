6e748f55640fa4edfdb2ae771705f7bde6bfbb63
will <will@spinspire.com>
2023-10-08 19:01:08 -0400

    Terminal kit breaking packaging with esbuild

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,31 @@
 #!/bin/bash
+import { $, question } from "zx";
 import { intro } from "./lib/sequences";
+import { terminal } from "terminal-kit";
+
+const history: string[] = []
+
+const table = [
+  ['History', 'map'],
+  [history.slice(-10).join('\n'), ''],
+  ['Command'],
+]
 
 async function main(): Promise<undefined> {
   process.env.FORCE_COLOR = '1'
-  await intro();
+  await $`clear`;
+
+  terminal.table(table, {
+    hasBorder: true,
+    width: terminal.width,
+    fit: true,
+  })
+
+  while (true) {
+    await question('> ');
+  }
+
+  // await intro();
 }
 
 try {
